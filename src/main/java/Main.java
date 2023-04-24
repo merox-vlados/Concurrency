a4e10e2f670b05c585ee731d936e585031c96dc4
merox-vlados <merox-vlados@mail.ru>
2023-04-24 18:00:47 +0300

    commit 24.04.2023

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 1000; i++) {
+                    System.out.print(i);
+                }
+            }
+        });
+        thread.start();
+        for(int i = 0; i < 1000; i++) {
+            System.out.print("M");
+        }
+        System.out.println("\nFinish");
+    }
 }
