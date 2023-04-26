586ea4b2e8ca5f2b8b49520ada69b39bfd580656
merox-vlados <merox-vlados@mail.ru>
2023-04-26 22:39:44 +0300

    commit 26.04.2023

--- src/main/java/ExecutorServiceHW.java
+++ src/main/java/ExecutorServiceHW.java
@@ -6,63 +6,62 @@ import java.util.concurrent.Executors;
 
 public class ExecutorServiceHW {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
         CountDownLatch countDownLatch = new CountDownLatch(3);
-        long before = System.currentTimeMillis();
-        executorService.execute(new Runnable() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Runnable task1 = new Runnable() {
             @Override
             public void run() {
-                int sum = 0;
-                for(int i = 0; i < 1_000_000; i ++) {
+                long sum = 0;
+                for(int i = 0; i < 10000000; i++) {
                     if(i % 2 == 0) {
                         sum += i;
                     }
                 }
-                System.out.println("Сумма всех чётных чисел: " + sum);
+                System.out.println("Task1: " + sum);
                 countDownLatch.countDown();
             }
-        });
-
-        executorService.execute(new Runnable() {
+        };
+        Runnable task2 = new Runnable() {
             @Override
             public void run() {
                 long sum = 0;
-                for(int i = 0; i < 1_000_000; i++) {
+                for(int i = 0; i < 10000000; i++) {
                     if(i % 7 == 0) {
                         sum += i;
                     }
                 }
-                System.out.println("Сумма всех чисел делящихся на 7 без остатка: " + sum);
+                System.out.println("Task2: " + sum);
                 countDownLatch.countDown();
             }
-        });
-        executorService.execute(new Runnable() {
-            List<Integer> randomNumbers = new ArrayList<>();
+        };
+        Runnable task3 = new Runnable() {
             @Override
             public void run() {
-                int count = 0;
+                List<Integer> numbers = new ArrayList<>();
                 for(int i = 0; i < 1000; i++) {
-                    double random = Math.random() * i;
-                    randomNumbers.add((int)random);
+                    numbers.add((int)(Math.random() * 1000));
                 }
-                for(Integer integer : randomNumbers) {
-                    if(integer % 2 == 0) {
+                int count = 0;
+                for(int i : numbers) {
+                    if(i % 2 == 0) {
                         count++;
                     }
                 }
-                System.out.println("Количество всех чётных чисел: " + count);
+                System.out.println("Task3: " + count);
                 countDownLatch.countDown();
             }
-        });
+        };
+        long before = System.currentTimeMillis();
+        executorService.execute(task1);
+        executorService.execute(task2);
+        executorService.execute(task3);
         executorService.shutdown();
-
         try {
             countDownLatch.await();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         long after = System.currentTimeMillis();
-        System.out.println("Время выполнения: " + (after-before));
-
+        System.out.println("Time: " + (after - before));
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,68 +1,53 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
-        CountDownLatch countDownLatch = new CountDownLatch(3);
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        Runnable task1 = new Runnable() {
+        ExecutorService executorService = Executors.newFixedThreadPool(3, new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setDaemon(true);
+                return thread;
+            }
+        });
+        executorService.execute(new Runnable() {
             @Override
             public void run() {
-                long sum = 0;
-                for(int i = 0; i < 10000000; i++) {
-                    if(i % 2 == 0) {
-                        sum += i;
+                try {
+                    while (true) {
+                        System.out.print(".");
+                        Thread.sleep(300);
                     }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
-                System.out.println("Task1: " + sum);
-                countDownLatch.countDown();
             }
-        };
-        Runnable task2 = new Runnable() {
+        });
+        Future<String> futureName =  executorService.submit(new Callable<String>() {
             @Override
-            public void run() {
-                long sum = 0;
-                for(int i = 0; i < 10000000; i++) {
-                    if(i % 7 == 0) {
-                        sum += i;
-                    }
-                }
-                System.out.println("Task2: " + sum);
-                countDownLatch.countDown();
+            public String call() throws Exception {
+                Thread.sleep(5000);
+                return "John";
             }
-        };
-        Runnable task3 = new Runnable() {
+        });
+        Future<Integer> futureAge = executorService.submit(new Callable<Integer>() {
             @Override
-            public void run() {
-                List<Integer> numbers = new ArrayList<>();
-                for(int i = 0; i < 1000; i++) {
-                    numbers.add((int)(Math.random() * 1000));
-                }
-                int count = 0;
-                for(int i : numbers) {
-                    if(i % 2 == 0) {
-                        count++;
-                    }
-                }
-                System.out.println("Task3: " + count);
-                countDownLatch.countDown();
+            public Integer call() throws Exception {
+                Thread.sleep(4000);
+                return 18;
             }
-        };
-        long before = System.currentTimeMillis();
-        executorService.execute(task1);
-        executorService.execute(task2);
-        executorService.execute(task3);
-        executorService.shutdown();
+        });
         try {
-            countDownLatch.await();
+            String name = futureName.get();
+            int age = futureAge.get();
+            System.out.println("\n Name: " + name + " age: " + age);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
         }
-        long after = System.currentTimeMillis();
-        System.out.println("Time: " + (after - before));
+
     }
 }
